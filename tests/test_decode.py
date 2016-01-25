2c0e9f4c7b52ff731cad18efb4f9e7a45195ce86
whtsky <whtsky@gmail.com>
2016-01-25 14:33:07 +0800

    Update decode test

--- tests/test_decode.py
+++ tests/test_decode.py
@@ -4,7 +4,7 @@ from bencoder import bdecode
 
 
 def test_decode_str():
-    assert bdecode(b'6:WWWWWW') == "WWWWWW"
+    assert bdecode(b'6:WWWWWW') == b"WWWWWW"
 
 
 def test_decode_int():
@@ -12,21 +12,21 @@ def test_decode_int():
 
 
 def test_decode_list():
-    assert bdecode(b'l1:a1:bi3ee') == ['a', 'b', 3]
+    assert bdecode(b'l1:a1:bi3ee') == [b'a', b'b', 3]
 
 
 def test_decode_dict():
     od = dict()
-    od['ka'] = 'va'
-    od['kb'] = 2
+    od[b'ka'] = b'va'
+    od[b'kb'] = 2
     assert bdecode(b'd2:ka2:va2:kbi2ee') == od
 
 
 def test_encode_complex():
     od = dict()
-    od['KeyA'] = ['listitemA', {'k': 'v'}, 3]
-    od['KeyB'] = {'k': 'v'}
-    od['KeyC'] = 3
-    od['KeyD'] = 'AString'
+    od[b'KeyA'] = [b'listitemA', {b'k': b'v'}, 3]
+    od[b'KeyB'] = {b'k': b'v'}
+    od[b'KeyC'] = 3
+    od[b'KeyD'] = b'AString'
     expected_result = b'd4:KeyAl9:listitemAd1:k1:vei3ee4:KeyBd1:k1:ve4:KeyCi3e4:KeyD7:AStringe'
     assert bdecode(expected_result) == od
