0d048bfcbf5bb9fa57b97353f7c90385d495b7cc
whtsky <whtsky@gmail.com>
2016-04-28 07:46:33 +0800

    Include bencoder.pyx in package.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ except ImportError:
 
 setup(
     name='bencoder.pyx',
-    version='1.1.0',
+    version='1.1.1',
     description='Yet another bencode implementation in Cython',
     long_description=open('README.rst', 'r').read(),
     author='whtsky',
