00bd6cefbf5f125bb9d187a71ad33d037fad0f52
whtsky <whtsky@users.noreply.github.com>
2016-04-29 16:30:18 +0800

    Fix build-wheels.sh

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -8,7 +8,7 @@ for PYBIN in /opt/python/*/bin; do
 done
 
 # Bundle external shared libraries into the wheels
-for whl in wheelhouse/bencoder*.whl; do
+for whl in wheelhouse/*.whl; do
     auditwheel repair $whl -w /io/wheelhouse/
 done
 
