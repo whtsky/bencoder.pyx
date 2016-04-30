6c1f3b740331508c8849ad9d49127466fde3e679
whtsky <whtsky@gmail.com>
2016-04-30 23:55:27 +0800

    Test first, compile wheels later on Travis.

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 set -e -x
 
-# Install a system package required by our library
-yum install -y atlas-devel
-
-# Compile wheels
-for PYBIN in /opt/python/*/bin; do
+# test and compile wheels
+for PYBIN in /opt/python/*/bin/; do
     ${PYBIN}/pip install -r /io/dev-requirements.txt
+    ${PYBIN}/python /io/setup.py test
     ${PYBIN}/pip wheel /io/ -w wheelhouse/
 done
 
 # Bundle external shared libraries into the wheels
-for whl in wheelhouse/*.whl; do
+for whl in wheelhouse/bencoder*.whl; do
     auditwheel repair $whl -w /io/wheelhouse/
 done
 
-# Install packages and test
-for PYBIN in /opt/python/*/bin/; do
-    ${PYBIN}/pip install bencoder.pyx --no-index -f /io/wheelhouse
-    ${PYBIN}/nosetests /io/
-done
