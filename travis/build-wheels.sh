5ef6c155dfa751b7e2ff566249b0d7a8c339b62d
whtsky <whtsky@gmail.com>
2016-05-01 00:39:37 +0800

    cd into source path before install.

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e -x
 
+cd /io
+
 # test and compile wheels
 for PYBIN in /opt/python/*/bin/; do
     ${PYBIN}/pip install -r /io/dev-requirements.txt
-    ${PYBIN}/python /io/setup.py test
-    ${PYBIN}/pip wheel /io/ -w wheelhouse/
+    ${PYBIN}/python setup.py test
+    ${PYBIN}/pip wheel /io/ -w /tmp/wheelhouse/
 done
 
 # Bundle external shared libraries into the wheels
-for whl in wheelhouse/bencoder*.whl; do
+for whl in /tmp/wheelhouse/bencoder*.whl; do
     auditwheel repair $whl -w /io/wheelhouse/
 done
-
