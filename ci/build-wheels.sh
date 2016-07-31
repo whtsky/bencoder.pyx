add0f43172ae0b36de80dd75f773369e7f09c18d
whtsky <me@tiantian.codes>
2016-07-31 22:53:43 +0800

    Run pytest on Linux CI

--- ci/build-wheels.sh
+++ ci/build-wheels.sh
@@ -3,9 +3,18 @@ set -e -x
 
 cd /io
 
-# test and compile wheels
+# tests
 for PYBIN in /opt/python/*/bin/; do
     ${PYBIN}/pip install -r /io/dev-requirements.txt
+    ${PYBIN}/python setup.py test
+done
+
+rm bencoder.c
+
+# compile wheels
+for PYBIN in /opt/python/*/bin/; do
+    ${PYBIN}/pip install -r /io/dev-requirements.txt
+    ${PYBIN}/cython bencoder.pyx
     ${PYBIN}/pip wheel /io/ -w /tmp/wheelhouse/
 done
 
@@ -16,7 +25,7 @@ done
 
 cp /tmp/wheelhouse/ordereddict* /io/wheelhouse
 
-# Install packages and test
+# Install packages and test again
 for PYBIN in /opt/python/*/bin/; do
     ${PYBIN}/pip install bencoder.pyx --no-index -f /io/wheelhouse
     (cd $/io; ${PYBIN}/py.test)
