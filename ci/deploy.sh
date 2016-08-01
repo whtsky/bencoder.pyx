4e283841e62706e3c418e4200eee1c957549eb7d
whtsky <me@tiantian.codes>
2016-08-01 23:28:39 +0800

    Install twine on AppVeyor

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo pip install twine
-sudo twine upload --username whtsky --password $PYPI_PASSWORD wheelhouse/bencoder*.whl
+pip install twine
+twine upload --username whtsky --password $PYPI_PASSWORD wheelhouse/bencoder*.whl
